6462d1a849362a4b9a2b53dfee811d6f19846a1f
jplukas <jplukasavicus@gmail.com>
2017-08-02 14:36:54 -0300

    Atualizacao

--- html/Admin/admin-viva-leve-home.php
+++ html/Admin/admin-viva-leve-home.php
@@ -1,13 +1,15 @@
 <?php include_once('header.php');?>
     <div class="conteudo col-md-9">
-        <?php include_once('slideshow.php');?>
+            <?php include_once('slideshow.php');?>
 
-        <?php include_once('logo_lead.php');?>
+            <?php include_once('logo_lead.php');?>
 
-        <?php include_once('o_que_fazemos.php');?>
+            <?php include_once('o_que_fazemos.php');?>
 
-        <?php include_once('parceiros.php');?>
+            <?php include_once('parceiros.php');?>
 
-        <?php include_once('duvidas.php');?>
+            <?php include_once('duvidas.php');?>
+            <br><br>
     </div>
+
 <?php include_once('footer.php');?>
--- html/Admin/slideshow.php
+++ html/Admin/slideshow.php
@@ -1,20 +1,22 @@
 <?php
     include_once('../../DAO/Slide_homeDAO.php');
+    include_once('../../DB/factory.php');
     $slides = Slide_homeDAO::readAll();
 ?>
                         <h2>SlideShow</h2>
                             <div id = "1">
-                                <form class="formulario-slideshow">
+                                <form class="formulario-slideshow" method="POST" action="teste.php">
                                     <p>Imagem principal (obrigatório)</p>
                                     <p class="flutuar">Imagem menor</p><br><br>
-                                    <input type="file" name="arquivos" class="btn files"/>
+                                    <input type="file" name="arquivos1" class="btn files"/>
                                     
-                                    <input type="file" name="arquivos" class="btn files"/><br><br>
+                                    <input type="file" name="arquivos2" class="btn files"/><br><br>
 
                                      <p>Slide Show - Texto maior</p>
-                                    <textarea></textarea><br><br>
+                                    <textarea name="Texto_maior"></textarea><br><br>
                                     <p>Slide Show - Texto menor</p>
-                                    <textarea></textarea><br><br>
+                                    <textarea name="Texto_menor"></textarea><br><br>
                                     <button class="btn ">Adicionar novo slide</button>
+                                    <input type="submit">
                                 </form>
                             </div>
